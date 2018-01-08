7784ae03fdc8c6a8eb3d37a5b4c9dfac650636a4
Matt Fenwick <mfenwick100@gmail.com>
2018-01-08 09:03:14 -0500

    run scan against scan.cli

--- scanner/hubscanclient.go
+++ scanner/hubscanclient.go
@@ -51,6 +51,55 @@ func (hsc *HubScanClient) FetchProject(projectName string) (*Project, error) {
 }
 
 func (hsc *HubScanClient) Scan(job ScanJob) error {
+	// TODO coupla problems here:
+	//   1. hardcoded path
+	//   2. hardcoded version number
+	scanCliImplJarPath := "./dependencies/scan.cli/lib/cache/scan.cli.impl-standalone.jar"
+	scanCliJarPath := "./dependencies/scan.cli/lib/scan.cli-4.5.0-SNAPSHOT-standalone.jar"
+	// path := job.ImageName // TODO how to figure out the path?
+	path := "./dependencies/centos7.tar" // TODO for now, we'll just hardcode a path to an image that we copied over
+	cmd := exec.Command("java",
+		"-Xms512m",
+		"-Xmx4096m",
+		"-Dblackduck.scan.cli.benice=true",
+		"-Dblackduck.scan.skipUpdate=true",
+		"-Done-jar.silent=true",
+		"-Done-jar.jar.path="+scanCliImplJarPath,
+		"-jar", scanCliJarPath,
+		"--host", hsc.host,
+		"--port", "443", // "--port", "8443", // TODO or should this be 8080 or something else? or should we just leave it off and let it default?
+		"--scheme", "https", // TODO or should this be http?
+		"--project", job.ProjectName,
+		// "--release", prefix, // TODO put something in here
+		"--username", hsc.username,
+		// "--name", codeLocation, // TODO maybe specify this based on the image sha or something
+		"--insecure", // TODO not sure about this
+		"-v",
+		path)
+	/*
+		cmd := exec.Command(hsc.pathToScanner,
+			"--image",
+			job.ImageName,
+			"--project",
+			job.ProjectName,
+			"--host",
+			hsc.host,
+			"--username",
+			hsc.username)
+	*/
+	fmt.Printf("running command %v for image %s\n", cmd, job.ImageName)
+	stdoutStderr, err := cmd.CombinedOutput()
+	if err != nil {
+		message := fmt.Sprintf("failed to run scan.cli.sh: %s", err.Error())
+		log.Error(message)
+		log.Errorf("output from scan.cli.sh:\n%v\n", string(stdoutStderr))
+		return err
+	}
+	log.Infof("successfully completed scan.cli.sh: %s", stdoutStderr)
+	return nil
+}
+
+func (hsc *HubScanClient) ScanDockerSh(job ScanJob) error {
 	cmd := exec.Command(hsc.pathToScanner, // "./scan.docker.sh", // imagename, host, username "get", "pods", "-o", "json", "--all-namespaces")
 		"--image",
 		job.ImageName,
