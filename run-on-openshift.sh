ccf6918dee4949d03ac23abe0b8678536e3e3658
Matt Fenwick <mfenwick100@gmail.com>
2018-01-10 14:13:54 -0500

    add yamls for running on openshift

---
+++ run-on-openshift.sh
@@ -0,0 +1,11 @@
+set -e
+
+oc create ns perceptor-scan
+oc create -f serviceaccount.yaml --namespace=perceptor-scan
+
+# allows writing of cluster level metadata for imagestreams
+# oc adm policy add-cluster-role-to-user cluster-admin system:serviceaccount:perceptor-scan:perceptor-scan
+# allows launching of privileged containers for Docker machine access
+oc adm policy add-scc-to-user privileged system:serviceaccount:perceptor-scan:perceptor-scan
+
+oc create -f perceptor.yaml --namespace=perceptor-scan
