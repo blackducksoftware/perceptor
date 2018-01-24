f0e2c9fca163e48a1fb09afe287590a8a1d39877
Matt Fenwick <mfenwick100@gmail.com>
2018-01-24 13:34:45 -0500

    extend build to support scanner container

--- build/deploy-openshift.sh
+++ build/deploy-openshift.sh
@@ -7,13 +7,14 @@ oc create -f ../cmd/kube-perceiver/kube-perceiverserviceaccount.yaml --namespace
 # allows writing of cluster level metadata for imagestreams
 oc adm policy add-cluster-role-to-user cluster-admin system:serviceaccount:bds-perceptor:kube-perceiver-sa
 
-oc create -f ../cmd/perceptor/perceptor-serviceaccount.yaml --namespace=bds-perceptor
+oc create -f ../cmd/perceptor-scanner/perceptor-scanner-serviceaccount.yaml --namespace=bds-perceptor
 # allows launching of privileged containers for Docker machine access
-oc adm policy add-scc-to-user privileged system:serviceaccount:bds-perceptor:perceptor-sa
+oc adm policy add-scc-to-user privileged system:serviceaccount:bds-perceptor:perceptor-scanner-sa
 
 
-oc create -f ../cmd/kube-perceiver/kube-perceiver.yaml --namespace=bds-perceptor
 oc create -f ../cmd/perceptor/perceptor.yaml --namespace=bds-perceptor
+oc create -f ../cmd/kube-perceiver/kube-perceiver.yaml --namespace=bds-perceptor
+oc create -f ../cmd/perceptor-scanner/perceptor-scanner.yaml --namespace=bds-perceptor
 
 
 oc create -f routes.yaml --namespace=bds-perceptor
