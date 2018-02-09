a629da1d0cea2b233f18f09ce01aa6443fdbdbbf
Matt Fenwick <mfenwick100@gmail.com>
2018-02-09 10:13:54 -0500

    remove obsoleted build files

--- cmd/perceptor-scanner/run-on-openshift.sh
+++
@@ -1,9 +0,0 @@
-set -e
-
-oc create ns bds-perceptor
-oc create -f perceptor-scanner-serviceaccount.yaml --namespace=bds-perceptor
-
-# allows launching of privileged containers for Docker machine access
-oc adm policy add-scc-to-user privileged system:serviceaccount:bds-perceptor:perceptor-scanner-sa
-
-oc create -f perceptor-scanner.yaml --namespace=bds-perceptor
--- cmd/perceptor/run-on-openshift.sh
+++
@@ -1,5 +0,0 @@
-set -e
-
-oc create ns bds-perceptor
-
-oc create -f perceptor.yaml --namespace=bds-perceptor
