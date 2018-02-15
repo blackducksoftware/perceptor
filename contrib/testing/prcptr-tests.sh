dc8a05a831458b8e0706642fcc05dae75dbf86c5
sheppduck <shepp67@gmail.com>
2018-02-15 12:30:23 -0500

    WIP: yaml to create namespace and script for automated tests/checks

--- contrib/testing/prcptr-tests.sh
+++ contrib/testing/prcptr-tests.sh
@@ -1,17 +1,32 @@
 # Copyright (C) 2018 Synopsys, Inc.
 #!/bin/sh
 
-export PERCEPTOR_POD_NAMESPACE="perceptorTestNS"
+export PERCEPTOR_POD_NS="perceptortestns"
 
 # TODO Put in a check here if kubectl cli is present
 
 # Create the Namespace
-kubectl create ns -f ./perceptorTestNS.yml
+createNs() {
+  WAIT_TIME=$((30))
+  # Clean up NS JIC it's still here...
+  kubectl get ns | grep $PERCEPTOR_POD_NS | xargs kubectl delete ns
+  sleep $WAIT_TIME
+  kubectl create -f ./perceptorTestNS.yml
+  sleep $WAIT_TIME
+  kubectl get ns | grep $PERCEPTOR_POD_NS
+  ns_state=$(kubectl get ns | grep $PERCEPTOR_POD_NS)
+  if [ -z $ns_state ] ; then
+    echo "Error: Namespace $PERCEPTOR_POD_NS not found!"
+    exit 1;
+  else
+    echo "Namespace $PERCEPTOR_POD_NS found, w00t! Moving on..."
+  fi
+}
 # Spin up a Kube POD using busybox
+createPod() {
 echo "Creating POD..."
-kubectl run busybox --image=busybox --namespace=$PERCEPTOR_POD_NAMESPACE
-echo "getting POD information"
-kubectl get pods | grep $PERCEPTOR_POD_NAMESPACE
+kubectl run busybox --image=busybox --namespace=$PERCEPTOR_POD_NS
+}
 
 # TODO Verify perceptor is notified of new POD/Image - not sure how yet...
 
@@ -20,7 +35,7 @@ tstAnnotate() {
   WAIT_TIME=$((30))
   echo "Checking for Blackduck POD annotations..."
   sleep $WAIT_TIME
-  a_state = $(kubectl describe pod $PERCEPTOR_POD_NAMESPACE | grep "blackduck")
+  a_state=$(kubectl describe pod $PERCEPTOR_POD_NS | grep "blackduck")
   if [[ -z $a_state ]]; then
     echo "There appears to be no POD Annoations present."
     exit 1;
@@ -29,4 +44,6 @@ tstAnnotate() {
   fi
 }
 
+createNs
+createPod
 tstAnnotate
