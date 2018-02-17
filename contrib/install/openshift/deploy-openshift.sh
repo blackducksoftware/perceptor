b9edc762c523531cf7d5e8c87f6f0ae578056cc9
Senthil Murugan Manikantan <smanikantan@blackducksoftware.com>
2018-02-16 19:54:43 -0500

    Copied the latest openshift-perceiver.yaml file and fixed the service account cluster role

--- contrib/install/openshift/deploy-openshift.sh
+++ contrib/install/openshift/deploy-openshift.sh
@@ -6,7 +6,7 @@ oc create ns bds-perceptor
 oc project bds-perceptor
 
 # Create the serviceaccount for perceptor-scanner to talk with Docker
-oc create -f perceptor-scanner-serviceaccount.yaml
+oc create serviceaccount perceptor-scanner-sa
 
 # allows launching of privileged containers for Docker machine access
 oc adm policy add-scc-to-user privileged system:serviceaccount:bds-perceptor:perceptor-scanner-sa
@@ -21,7 +21,7 @@ oc create -f perceptor-scanner.yaml
 oc create serviceaccount openshift-perceiver
 
 # following allows us to write cluster level metadata for imagestreams
-oc adm policy add-cluster-role-to-user cluster-admin system:serviceaccount::openshift-perceiver
+oc adm policy add-cluster-role-to-user cluster-admin system:serviceaccount:bds-perceptor:openshift-perceiver
 
 # Create the perceptor-scanner container
 oc create -f openshift-perceiver.yaml
