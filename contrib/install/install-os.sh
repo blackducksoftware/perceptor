c8944de5ee174abddde469ba1fecb552a950d7ae
jayunit100 <jay@apache.org>
2018-02-23 13:22:26 +0000

    working installer w00t

--- contrib/install/install-os.sh
+++ contrib/install/install-os.sh
@@ -1,42 +1,92 @@
 #!/bin/bash 
+
+NS=bds-perceptor
+KUBECTL="kubectl"
+
+function is_openshift {
+	oc version	
+	return
+}
+
+cleanup() {
+	is_openshift
+	if ! $(exit $?); then
+		echo "assuming kube"
+		KUBECTL="kubectl"
+		$KUBECTL delete ns $NS
+                $KUBECTL delete sa perceptor-sanner-sa
+	else 
+		KUBECTL="oc"
+		if oc get ns | grep -q bds-perceptor ; then
+			echo "deleting pereptor project!!!"
+			sleep 2
+			oc delete project $NS
+		fi
+		oc delete sa perceptor-scanner-sa
+	fi
+	while oc get ns | grep -q $NS ; do
+	    echo "Waiting for deletion...`$KUBECTL get ns | grep perc` "
+	    sleep 1
+        done
+}
+
+install() {
+	is_openshift
+	if ! $(exit $?); then
+	    echo "assuming kube"
+	    $KUBECTL create sa perceptor-scanner-sa
+	    $KUBECTL create ns $NS
+	else
+	    set -e
+	    KUBECTL="oc"
+	    echo "Detected openshift... setting up "
+	    # Create the namespace to install all containers
+	    oc new-project $NS
+		
+	    pushd openshift/ 
+			# Create the openshift-perceiver service account
+			oc create serviceaccount openshift-perceiver
+
+			# following allows us to write cluster level metadata for imagestreams
+			oc adm policy add-cluster-role-to-user cluster-admin system:serviceaccount:bds-perceptor:openshift-perceiver
+
+			# Create the serviceaccount for perceptor-scanner to talk with Docker
+			oc create sa perceptor-scanner-sa
+
+			# allows launching of privileged containers for Docker machine access
+			oc adm policy add-scc-to-user privileged system:serviceaccount:bds-perceptor:perceptor-scanner-sa
+
+			# Create the perceptor container
+			oc create -f openshift-perceiver.yaml
+	    popd
+	fi
+}
+
+install-contrib() {
+	set -e
+	#### 
+	#### The perceptor core functionality 
+	####
+
+	pushd kube/
+		$KUBECTL create -f kube-perceiver.yaml --namespace=$NS
+		$KUBECTL create -f perceptor-scanner.yaml --namespace=$NS
+		$KUBECTL create -f perceptor.yaml --namespace=$NS
+	popd
+
+	# Deploy a small, local prometheus.  It is only used for scraping perceptor.  Doesnt need fancy ACLs for
+	# cluster discovery etc.
+	pushd prometheus/
+		$KUBECTL create -f prom.cfg.yml  
+		$KUBECTL create -f prometheus-deployment.yaml
+	popd
+}
+
+cleanup
+set -x
 set -e
-
-# Create the namespace to install all containers
-oc create ns bds-perceptor
-oc project bds-perceptor
-
-
-# Deploy a small, local prometheus.  It is only used for scraping perceptor.  Doesnt need fancy ACLs for
-# cluster discovery etc.
-pushd prometheus/
-	oc create -f prom.cfg.yml  
-	oc create -f prometheus-deployment.yaml
-popd
-
-# Main installation of perceptor stack.
-pushd openshift/ 
-	# Create the openshift-perceiver service account
-	oc create serviceaccount openshift-perceiver
-
-	# following allows us to write cluster level metadata for imagestreams
-	oc adm policy add-cluster-role-to-user cluster-admin system:serviceaccount:bds-perceptor:openshift-perceiver
-
-	# Create the serviceaccount for perceptor-scanner to talk with Docker
-	oc create serviceaccount perceptor-scanner-sa
-
-	# allows launching of privileged containers for Docker machine access
-	oc adm policy add-scc-to-user privileged system:serviceaccount:bds-perceptor:perceptor-scanner-sa
-
-	# Create the perceptor container
-	oc create -f perceptor.yaml
-
-	# Create the perceptor-scanner container
-	oc create -f perceptor-scanner.yaml
-	# Create the perceptor-scanner container
-	oc create -f openshift-perceiver.yaml
-
-	#oc create -f routes.yaml --namespace=bds-perceptor
-popd
-
-
+install
+set +e
+echo "optional install components starting now..."
+install-contrib
 
