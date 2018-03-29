e808765d387823497e9849d5371d9dfe0f588574
Senthil <manikan@synopsys.com>
2018-03-29 08:28:29 -0400

    Inject Hub password from Viper-secret

--- pkg/core/model/config.go
+++ pkg/core/model/config.go
@@ -49,6 +49,15 @@ func GetConfig() (*Config, error) {
 	var config *Config
 
 	viper.SetConfigName("perceptor_conf")
+
+	// these need to be set before we read in the config!
+	viper.SetEnvPrefix("PCP")
+	viper.BindEnv("HubUserPassword")
+	if viper.GetString("hubuserpassword") == "" {
+		viper.Debug()
+		panic("No hub database password secret supplied.  Please inject PCP_HUBUSERPASSWORD as a secret and restart!")
+	}
+
 	viper.AddConfigPath("/etc/perceptor")
 
 	err := viper.ReadInConfig()
