9b74448f9ff39154af31ff8587b7cf72af01faef
Matt Fenwick <mfenwick100@gmail.com>
2018-05-29 13:07:43 -0400

    add unit tests

--- pkg/core/actions/getnextimage_test.go
+++ pkg/core/actions/getnextimage_test.go
@@ -70,3 +70,35 @@ func TestGetNextImage(t *testing.T) {
 	assertEqual(t, model.Images[image1.Sha].ScanStatus, m.ScanStatusRunningScanClient)
 	// TODO expected: time of image changed
 }
+
+// TestGetNextImageHubInaccessible .....
+func TestGetNextImageHubInaccessible(t *testing.T) {
+	model := m.NewModel("test version", &m.Config{ConcurrentScanLimit: 3}, nil)
+	model.AddImage(image1)
+	model.SetImageScanStatus(image1.Sha, m.ScanStatusInQueue)
+	model.IsHubEnabled = false
+
+	var nextImage *m.Image
+	var wg sync.WaitGroup
+	wg.Add(1)
+	(&GetNextImage{func(image *m.Image) {
+		nextImage = image
+		wg.Done()
+	}}).Apply(model)
+	wg.Wait()
+
+	if nextImage != nil {
+		t.Errorf("expected nil, got %+v", nextImage)
+	}
+
+	model.IsHubEnabled = true
+	wg.Add(1)
+	(&GetNextImage{func(image *m.Image) {
+		nextImage = image
+		wg.Done()
+	}}).Apply(model)
+	wg.Wait()
+	if nextImage == nil {
+		t.Errorf("expected image, got nil")
+	}
+}
