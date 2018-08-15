35c5db042d32bf8da0a4211a2a07828f47e6a71a
Matt Fenwick <mfenwick100@gmail.com>
2018-08-15 16:39:20 -0400

    rename scheduler to timer

--- pkg/util/timer_test.go
+++ pkg/util/timer_test.go
@@ -2,7 +2,7 @@
 Copyright (C) 2018 Synopsys, Inc.
 
 Licensed to the Apache Software Foundation (ASF) under one
-or more contributor license agreements. See the NOTICE file
+or more contributor license agreementtimer. See the NOTICE file
 distributed with this work for additional information
 regarding copyright ownership. The ASF licenses this file
 to you under the Apache License, Version 2.0 (the
@@ -29,14 +29,14 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-var _ = Describe("Scheduler", func() {
+var _ = Describe("Timer", func() {
 	It("Pause before completion", func() {
 		stop := make(chan struct{})
 		defer close(stop)
 		x := 0
-		s := NewRunningScheduler("test1", 1*time.Second, stop, false, func() { x++ })
+		timer := NewRunningTimer("test1", 1*time.Second, stop, false, func() { x++ })
 		time.Sleep(500 * time.Millisecond)
-		err := s.Pause()
+		err := timer.Pause()
 		Expect(err).To(BeNil())
 		Expect(x).To(Equal(0))
 	})
@@ -45,9 +45,9 @@ var _ = Describe("Scheduler", func() {
 		stop := make(chan struct{})
 		defer close(stop)
 		x := 0
-		s := NewRunningScheduler("test2", 1*time.Second, stop, false, func() { x++ })
+		timer := NewRunningTimer("test2", 1*time.Second, stop, false, func() { x++ })
 		time.Sleep(1500 * time.Millisecond)
-		err := s.Pause()
+		err := timer.Pause()
 		Expect(err).To(BeNil())
 		Expect(x).To(Equal(1))
 	})
@@ -56,8 +56,8 @@ var _ = Describe("Scheduler", func() {
 		stop := make(chan struct{})
 		defer close(stop)
 		x := 0
-		s := NewRunningScheduler("test3", 1*time.Second, stop, false, func() { x++ })
-		log.Debugf("started s: %+v", s)
+		timer := NewRunningTimer("test3", 1*time.Second, stop, false, func() { x++ })
+		log.Debugf("started timer: %+v", timer)
 		time.Sleep(1500 * time.Millisecond)
 		Skip("Pausing after stopping is currently not supported")
 	})
@@ -70,10 +70,10 @@ var _ = Describe("Scheduler", func() {
 		stop := make(chan struct{})
 		defer close(stop)
 		x := 0
-		s := NewRunningScheduler("test4", 250*time.Millisecond, stop, false, func() { x++ })
+		timer := NewRunningTimer("test4", 250*time.Millisecond, stop, false, func() { x++ })
 		log.Debug("middle")
 		time.Sleep(650 * time.Millisecond)
-		err := s.Pause()
+		err := timer.Pause()
 		Expect(err).To(BeNil())
 		Expect(x).To(Equal(2))
 		log.Debug("end")
@@ -83,10 +83,10 @@ var _ = Describe("Scheduler", func() {
 		stop := make(chan struct{})
 		defer close(stop)
 		x := 0
-		s := NewRunningScheduler("test5", 250*time.Millisecond, stop, true, func() { x++ })
+		timer := NewRunningTimer("test5", 250*time.Millisecond, stop, true, func() { x++ })
 		log.Debug("middle")
 		time.Sleep(650 * time.Millisecond)
-		err := s.Pause()
+		err := timer.Pause()
 		Expect(err).To(BeNil())
 		Expect(x).To(Equal(3))
 		log.Debug("end")
@@ -98,7 +98,7 @@ var _ = Describe("Scheduler", func() {
 		x := 0
 		y := 0
 		useX := true
-		s := NewRunningScheduler("test6", 500*time.Millisecond, stop, true, func() {
+		timer := NewRunningTimer("test6", 500*time.Millisecond, stop, true, func() {
 			if useX {
 				x++
 			} else {
@@ -106,11 +106,11 @@ var _ = Describe("Scheduler", func() {
 			}
 		})
 		time.Sleep(250 * time.Millisecond)
-		Expect(s.Pause()).To(BeNil())
+		Expect(timer.Pause()).To(BeNil())
 		Expect(x).To(Equal(1))
 		Expect(y).To(Equal(0))
 		useX = false
-		Expect(s.Resume(true)).To(BeNil())
+		Expect(timer.Resume(true)).To(BeNil())
 		time.Sleep(750 * time.Millisecond)
 		Expect(x).To(Equal(1))
 		Expect(y).To(Equal(2))
@@ -120,64 +120,64 @@ var _ = Describe("Scheduler", func() {
 		stop := make(chan struct{})
 		defer close(stop)
 		x := 0
-		s := NewRunningScheduler("test7", 2*time.Second, stop, true, func() {
+		timer := NewRunningTimer("test7", 2*time.Second, stop, true, func() {
 			time.Sleep(1 * time.Second)
 			x++
 		})
 		time.Sleep(500 * time.Millisecond)
-		Expect(s.state).To(Equal(SchedulerStateRunningAction))
+		Expect(timer.state).To(Equal(TimerStateRunningAction))
 		Expect(x).To(Equal(0))
 		time.Sleep(1 * time.Second)
-		Expect(s.state).To(Equal(SchedulerStateReady))
+		Expect(timer.state).To(Equal(TimerStateReady))
 		Expect(x).To(Equal(1))
 	})
 
 	It("stops executing action after being stopped", func() {
 		stop := make(chan struct{})
 		x := 0
-		s := NewRunningScheduler("test8", 500*time.Millisecond, stop, false, func() {
+		timer := NewRunningTimer("test8", 500*time.Millisecond, stop, false, func() {
 			log.Errorf("this shouldn't get executed")
 			x++
 		})
 		time.Sleep(250 * time.Millisecond)
 		close(stop)
 		time.Sleep(10 * time.Millisecond)
-		Expect(s.state).To(Equal(SchedulerStateStopped))
+		Expect(timer.state).To(Equal(TimerStateStopped))
 		time.Sleep(2 * time.Second)
 		Expect(x).To(Equal(0))
-		Expect(s.state).To(Equal(SchedulerStateStopped))
+		Expect(timer.state).To(Equal(TimerStateStopped))
 		// wait a bit longer to make sure it's not running
 		time.Sleep(2 * time.Second)
 		Expect(x).To(Equal(0))
-		Expect(s.state).To(Equal(SchedulerStateStopped))
+		Expect(timer.state).To(Equal(TimerStateStopped))
 	})
 
 	It("can be stopped while running action -- action will complete, then it will stop", func() {
 		stop := make(chan struct{})
 		beforeSleep := 0
 		afterSleep := 0
-		s := NewRunningScheduler("test9", 2*time.Second, stop, true, func() {
+		timer := NewRunningTimer("test9", 2*time.Second, stop, true, func() {
 			beforeSleep++
 			time.Sleep(1 * time.Second)
 			afterSleep++
 		})
 		time.Sleep(1 * time.Millisecond)
-		Expect(s.state).To(Equal(SchedulerStateRunningAction))
+		Expect(timer.state).To(Equal(TimerStateRunningAction))
 		Expect(beforeSleep).To(Equal(1))
 		Expect(afterSleep).To(Equal(0))
 		// now, cancel it while the action is running
 		time.Sleep(500 * time.Millisecond)
-		Expect(s.state).To(Equal(SchedulerStateRunningAction))
+		Expect(timer.state).To(Equal(TimerStateRunningAction))
 		close(stop)
-		Expect(s.state).To(Equal(SchedulerStateRunningAction))
+		Expect(timer.state).To(Equal(TimerStateRunningAction))
 		// wait for the action to complete ... even though that shouldn't make a difference
 		time.Sleep(750 * time.Millisecond)
-		Expect(s.state).To(Equal(SchedulerStateStopped))
+		Expect(timer.state).To(Equal(TimerStateStopped))
 		Expect(beforeSleep).To(Equal(1))
 		Expect(afterSleep).To(Equal(1))
 		// wait a bit longer to make sure it's not running
 		time.Sleep(2 * time.Second)
-		Expect(s.state).To(Equal(SchedulerStateStopped))
+		Expect(timer.state).To(Equal(TimerStateStopped))
 		Expect(beforeSleep).To(Equal(1))
 		Expect(afterSleep).To(Equal(1))
 	})
@@ -186,24 +186,24 @@ var _ = Describe("Scheduler", func() {
 		stop := make(chan struct{})
 		beforeSleep := 0
 		afterSleep := 0
-		s := NewRunningScheduler("test10", 1*time.Second, stop, true, func() {
+		timer := NewRunningTimer("test10", 1*time.Second, stop, true, func() {
 			beforeSleep++
 			time.Sleep(1 * time.Second)
 			afterSleep++
 		})
 		time.Sleep(1 * time.Millisecond)
-		Expect(s.state).To(Equal(SchedulerStateRunningAction))
+		Expect(timer.state).To(Equal(TimerStateRunningAction))
 		Expect(beforeSleep).To(Equal(1))
 		Expect(afterSleep).To(Equal(0))
 		// now, cancel it while the action is running
 		time.Sleep(250 * time.Millisecond)
-		Expect(s.state).To(Equal(SchedulerStateRunningAction))
+		Expect(timer.state).To(Equal(TimerStateRunningAction))
 		close(stop)
 		time.Sleep(3 * time.Millisecond)
-		Expect(s.state).To(Equal(SchedulerStateStopped))
+		Expect(timer.state).To(Equal(TimerStateStopped))
 		// wait a bit longer to make sure it's not running
 		time.Sleep(3 * time.Second)
-		Expect(s.state).To(Equal(SchedulerStateStopped))
+		Expect(timer.state).To(Equal(TimerStateStopped))
 		Expect(beforeSleep).To(Equal(1))
 		Expect(afterSleep).To(Equal(1))
 	})
