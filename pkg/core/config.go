3531ec6e2f3c40e412b48e1a6d829366cb5b7658
Senthil Murugan <msenmurugan@gmail.com>
2018-08-27 16:21:44 -0400

    Formatted the code

--- pkg/core/config.go
+++ pkg/core/config.go
@@ -62,11 +62,11 @@ func (config *Config) GetLogLevel() (log.Level, error) {
 // so viper is aware of the new structure
 func (config *Config) GetDefaults() map[string]interface{} {
 	defaults := Config{
-		HubHost:                         "",
-		HubUser:                         "",
-		HubUserPasswordEnvVar:           "",
-		HubClientTimeoutMilliseconds:    5000,
-		HubPort:                         443,
+		HubHost:                      "",
+		HubUser:                      "",
+		HubUserPasswordEnvVar:        "",
+		HubClientTimeoutMilliseconds: 5000,
+		HubPort: 443,
 		PruneOrphanedImagesPauseMinutes: 20,
 		ConcurrentScanLimit:             1,
 		UseMockMode:                     false,
