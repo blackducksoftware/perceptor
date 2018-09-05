5aeee8416408934e8a95b162e325dacc2eba4bf2
Matt Fenwick <mfenwick100@gmail.com>
2018-09-05 12:07:35 -0400

    revert accidental change

--- pkg/util/timer_test.go
+++ pkg/util/timer_test.go
@@ -2,7 +2,7 @@
 Copyright (C) 2018 Synopsys, Inc.
 
 Licensed to the Apache Software Foundation (ASF) under one
-or more contributor license agreementtimer. See the NOTICE file
+or more contributor license agreements. See the NOTICE file
 distributed with this work for additional information
 regarding copyright ownership. The ASF licenses this file
 to you under the Apache License, Version 2.0 (the
