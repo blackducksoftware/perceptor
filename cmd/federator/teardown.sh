9e14eb6d1fa1e795b321b92fb1c0808e1f949931
Matt Fenwick <mfenwick100@gmail.com>
2018-12-10 13:43:34 -0500

    remove unused code

--- cmd/federator/teardown.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-NS=$1
-
-kubectl delete -f federator-configmap.yaml -n $NS
-kubectl delete -f federator.yaml -n $NS
-kubectl delete -f federator-service.yaml -n $NS
-kubectl delete service federator-public -n $NS
